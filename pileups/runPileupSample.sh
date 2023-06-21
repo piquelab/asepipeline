124d7096ff5b0a57399349d5e4c7dbade2e59325
rpique <rpique@gmail.com>
2023-06-21 17:08:35 -0400

    Making a version where genotypes are not available

--- pileups/runPileupSample.sh
+++ pileups/runPileupSample.sh
@@ -15,22 +15,26 @@ set -v
 
 sample=$1
 
-vcfFileMain=/wsu/home/groups/piquelab/barreiro2/genotypes/filtered.vcf.gz
+##vcfFileMain=/wsu/home/groups/piquelab/barreiro2/genotypes/filtered.vcf.gz
 fastaRef=/wsu/home/groups/piquelab/barreiro2/ref/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa
 bamFolder=/nfs/osiris-pique/barreiro2/bams/
 ncores=8
 
+dbSNP=/nfs/rprdata/RefSnps/dbSNP144.hg38/snp144Common.bed.gz
 
-bcftools view -s $sample $vcfFileMain \
-  | bcftools view -g het \
-  | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\n' \
-  > $sample.het.pos.txt
+##less /nfs/rprdata/RefSnps/dbSNP144.hg38/snp144Common.bed.gz | head
+##1       11007   11008   rs575272151     C       G       0.088059
+
+#bcftools view -s $sample $vcfFileMain \
+#  | bcftools view -g het \
+#  | bcftools query -f '%CHROM\t%POS\t%ID\t%REF\t%ALT\n' \
+#  > $sample.het.pos.txt
 
 ##bcftools view -s AF04 ../../genotypes/filtered.vcf.gz | bcftools view -g het -Oz > AF04.het.vcf.gz
 
 time bcftools mpileup -d 1000000 --min-MQ 30 --min-BQ 30 \
                       -a FORMAT/AD,FORMAT/ADF,FORMAT/ADR,FORMAT/DP,FORMAT/SP,INFO/AD,INFO/ADF,INFO/ADR \
-                      -f $fastaRef -T $sample.het.pos.txt \
+                      -f $fastaRef -T ${dbSNP} \
                       --threads $ncores \
                       ${bamFolder}/*/${sample}*bam \
      | bcftools call -m -Oz --threads $ncores \
